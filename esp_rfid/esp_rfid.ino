5613289405013a053480b15200ea1a98749fa086
Lovaniainacarlx <ralaivaocarl128@gmail.com>
2026-03-26 23:33:41 +0300

    Initialisation du projet RFID (ESP32 + Backend + GUI)

---
+++ esp_rfid/esp_rfid.ino
@@ -0,0 +1,16 @@
+#include <MFRC5.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Initialisation esp..");
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Serial.println("9a marche e");
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
